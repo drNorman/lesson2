c65c1399bc19b98a2387899c531e0598bb6362d7
drNorman <71461772+drNorman@users.noreply.github.com>
2024-08-31 15:41:41 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,6 @@
+completed_homework = 12
+spent_time = 1.5
+course_name = 'Python'
+average_spent_time = spent_time / completed_homework
+
+print('Курс:', course_name, ',всего задач:', completed_homework, ',затрачено часов:', spent_time, ',среднее время выполнения', average_spent_time, 'часа.')
